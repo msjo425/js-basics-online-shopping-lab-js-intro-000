c182a373d404620a4fe4cadcd3b906045de3b4ba
matt joseloff <msjo425@gmail.com>
2020-05-08 19:45:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function setCart(c) {
 }
 
 function addToCart(item) {
-cart.push({itemName:item, itemPrice:Math.random(1,101)})
+cart.push({itemName:item, itemPrice:Math.floor(Math.random(1,101)})
 
 }
 
