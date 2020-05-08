abdfb48c195d894d04df7327ef22f6991232caa5
matt joseloff <msjo425@gmail.com>
2020-05-08 21:02:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ return `That item is not in your cart.`
 }
 
 function placeOrder(cardNumber) {
-if (cardNumber == "") {
+if (cardNumber === undefined {
   return `Sorry, we don\'t have a card on file for you.`
 }
 }
