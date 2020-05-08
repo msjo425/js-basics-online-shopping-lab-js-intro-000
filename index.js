bcfa99cc7d59e917d3833baf6400792ad8d1e084
matt joseloff <msjo425@gmail.com>
2020-05-08 21:05:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -56,6 +56,6 @@ if (cardNumber === undefined) {
   return `Sorry, we don't have a credit card on file for you.`
 }
 else {
-  return `Your total cost is $${total()}, which will be charged to the cart ${cardNumber}`
+  return `Your total cost is $${total()}, which will be charged to the cart ${cardNumber}.`
 }
 }
